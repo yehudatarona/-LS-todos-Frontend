03a4fe9af7104374460a413ea98b9d1547ba7821
Yehuda Tarona <yehuda.tarona@gmail.com>
2020-09-03 14:37:49 +0300

    03 09 2020

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import AppSites from './yehudaSites_comps/appSites';
+
 
 function App() {
   return (
     <div className="App">
-     <AppSites/>
+     app works
     </div>
   );
 }
